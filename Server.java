9bbb5864fca8cee9302b213036f8de8c3575b24a
Andrew Dempsey <andrewdempsey2016@gmail.com>
2018-06-11 11:44:07 +0100

    Signed-off-by: Andrew Dempsey <andrewdempsey2016@gmail.com>
    About to roll back to try plane and transmitter on different ports

--- Server.java
+++ Server.java
@@ -4,89 +4,64 @@ import java.net.*;
 public class Server
 {
     static final int PORT = 2000;
-	Connection connection;
+		
+	ServerSocket ss;
+	Socket socket;
+
+	private DataOutputStream os;
+	private DataInputStream is;
 
 	public Server()
 	{
 		System.out.println("Server has started...");
-	}
+
+		try
+		{
+			ss = new ServerSocket(PORT);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+}
 
 	public void runProgram()
 	{
 		try
 		{
-			ServerSocket ss = new ServerSocket(PORT);
-
-			while(true)
-			{
-				// wait for a connection request
-				Socket socket = ss.accept();
-				
-				
-
-				connection = new Connection(socket); 
-				connection.start();
-				System.out.println("A client has connected...");
-            } 
+			socket = ss.accept();
+			openStreams();
+			processMessages();
+			closeStreams();
 		}
-		catch (Exception e) 
-		{ 
-			System.out.println("Trouble with a connection " + e); 
-		}
-		
-	}
-
-	public class Connection extends Thread
-    {
-		private Socket socket;
-		private DataOutputStream os;
-		private DataInputStream is;
-		private boolean running;
-
-		private int value;
-
-		public Connection(Socket aSocket)
-		{
-			
-			socket = aSocket;
-
-			try
-			{
-				os = new DataOutputStream(socket.getOutputStream());
-				is = new DataInputStream(socket.getInputStream());
-			}
-			catch(Exception e)
-			{
-				value = 0;
-				running = true;
-			}
-		}
-	
-		public void run()
-		{
-			while(running)
-			{
-				try
-				{
-					value = is.readInt();
-					os.writeInt(value);
-				}
-				catch(Exception e)
-				{
-					System.out.println(e);
-				}
-			}
-			
-			try
-			{
-				os.close();
-				is.close();
-				socket.close();
-			}
-			catch(Exception e)
-			{
-				System.out.println(e);
-			}
+		catch(IOException ioe)
+		{	
+			System.out.println(ioe);
 		}
 	}
+
+	private void openStreams() throws IOException
+	{
+		is = new DataInputStream(socket.getInputStream());
+		os = new DataOutputStream(socket.getOutputStream());
+	}
+
+	private void closeStreams() throws IOException
+	{
+		is.close();
+		os.close();
+	}
+
+	private void processMessages() throws IOException
+	{
+		int value = 0;
+
+		while(value != 7) //value 7 from transmitter = socket close
+		{
+			value = is.readInt();
+			os.writeInt(value);
+			System.out.println(value);
+		}
+	}
+
 }
