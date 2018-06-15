2c250fe83601fe1f9299d52883d67d045eb884b5
Andrew Dempsey <andrewdempsey2016@gmail.com>
2018-06-15 21:59:30 +0100

    Signed-off-by: Andrew Dempsey <andrewdempsey2016@gmail.com>
    Begining to improve transmitter features

--- Plane.java
+++ Plane.java
@@ -29,20 +29,78 @@ public class Plane
 		}
 	}
 
-	public void testRunProgram()
+	public void test_runProgram()
 	{
-		System.out.println("Plane got here");
-		while(true)
+        try
 		{
-			try
+			int valueFromServer = 0;
+
+			//initialise servos to 50%
+			//runTime.exec(new String[] {"bash", "-c", "echo 1=50% > /dev/servoblaster"});
+			//runTime.exec(new String[] {"bash", "-c", "echo 3=50% > /dev/servoblaster"});
+
+			while(socket.isConnected())
 			{
-				System.out.println("" + is.readInt());
-				os.writeInt(666);
+                valueFromServer = is.readInt();
+
+				if(valueFromServer == 1)
+				{
+					System.out.println("elevator up: +10");
+			        //runTime.exec(new String[] {"bash", "-c", "echo 1=+10 > /dev/servoblaster"});
+				}
+
+				if(valueFromServer == 2)
+				{
+					System.out.println("elevator center: 50%");
+					//runTime.exec(new String[] {"bash", "-c", "echo 1=50% > /dev/servoblaster"});
+				}
+
+				if(valueFromServer == 3)
+				{
+					System.out.println("elevator down: -10");
+					//runTime.exec(new String[] {"bash", "-c", "echo 1=-10 > /dev/servoblaster"});
+				}
+
+                if(valueFromServer == 4)
+				{
+					System.out.println("rudder left: +10");
+					//runTime.exec(new String[] {"bash", "-c", "echo 3=+10 > /dev/servoblaster"});
+				}
+
+				if(valueFromServer == 5)
+				{
+					System.out.println("rudder center: 50%");
+					//runTime.exec(new String[] {"bash", "-c", "echo 3=50% > /dev/servoblaster"});
+				}
+
+				if(valueFromServer == 6)
+				{
+					System.out.println("rudder right: -10");
+					//runTime.exec(new String[] {"bash", "-c", "echo 3=-10 > /dev/servoblaster"});
+				}
+
+				if(valueFromServer == 7)
+				{
+					try
+			        {
+				        socket.close();
+			        }
+			        catch(IOException ioe)
+			        {
+				        System.out.println(ioe);
+			        }
+				}
+
+				if(valueFromServer == 8)
+				{
+					System.out.println("shutting down airplane...");
+					//runTime.exec("sudo shutdown -h now");
+				}
 			}
-			catch(Exception e)
-			{
-				System.out.println("trouble with plane: " + e);
-			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Connection lost " + e);
 		}
 	}
 
